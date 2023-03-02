a9dfcd3e872b2fa5b64a08f5df422a521067eab5
Alistair Israel <aisrael@gmail.com>
2023-03-02 13:04:19 -0500

    Added TailwindCSS typography

--- apps/gitsudo_web/assets/tailwind.config.js
+++ apps/gitsudo_web/assets/tailwind.config.js
@@ -13,15 +13,17 @@ module.exports = {
     extend: {
       colors: {
         brand: "#FD4F00",
+        primary: "#266AE8"
       }
     },
   },
   plugins: [
-    require("daisyui"),
     require("@tailwindcss/forms"),
-    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
   ]
 }
