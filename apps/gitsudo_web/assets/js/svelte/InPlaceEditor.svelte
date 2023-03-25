0e9fcea5dc67416e89adfbbb1ad43958bc20ba5d
Alistair Israel <aisrael@gmail.com>
2023-03-24 22:59:54 -0400

    First cut of Svelte in-place editor

---
+++ apps/gitsudo_web/assets/js/svelte/InPlaceEditor.svelte
@@ -0,0 +1,25 @@
+<script>
+    let editing = false;
+    export let text;
+
+    function toggleEditing(e) {
+        editing = !editing;
+    }
+</script>
+
+{#if editing}
+    <input type="text" class="input input-bordered" placeholder={text} />
+    <button class="btn btn-xs btn-primary">
+        <span class="hero-check-circle" />
+    </button>
+    <button class="btn btn-xs btn-ghost" on:click={toggleEditing}>
+        <span class="hero-x-circle" />
+    </button>
+{:else}
+    <input
+        type="text"
+        class="input input-ghost min-w-xs w-xs"
+        placeholder={text}
+    />
+    <span class="hero-pencil-square" on:click={toggleEditing} />
+{/if}
