7921ab10e313b7f440032e4d86e798290ad170a9
Alistair Israel <aisrael@gmail.com>
2023-03-27 10:38:41 -0400

    Getting this styling thing

--- apps/gitsudo_web/assets/js/svelte/InPlaceEditor.svelte
+++ apps/gitsudo_web/assets/js/svelte/InPlaceEditor.svelte
@@ -7,19 +7,30 @@
     }
 </script>
 
-{#if editing}
-    <input type="text" class="input input-bordered" placeholder={text} />
-    <button class="btn btn-xs btn-primary">
-        <span class="hero-check-circle" />
-    </button>
-    <button class="btn btn-xs btn-ghost" on:click={toggleEditing}>
-        <span class="hero-x-circle" />
-    </button>
-{:else}
-    <input
-        type="text"
-        class="input input-ghost min-w-xs w-xs"
-        placeholder={text}
-    />
-    <span class="hero-pencil-square" on:click={toggleEditing} />
-{/if}
+<div class="align-top">
+    <h1>
+        {#if editing}
+            <input
+                type="text"
+                class="input input-bordered"
+                placeholder={text}
+            />
+            <button class="btn btn-xs btn-circle btn-confirm">
+                <span class="hero-check-circle" />
+            </button>
+            <button
+                class="btn btn-xs btn-circle btn-cancel"
+                on:click={toggleEditing}
+            >
+                <span class="hero-x-circle" />
+            </button>
+        {:else}
+            {text}
+            <button
+                class="btn btn-xs btn-circle btn-ghost text-neutral-focus hover:bg-transparent"
+                on:click={toggleEditing}
+                ><span class="hero-pencil-square" />
+            </button>
+        {/if}
+    </h1>
+</div>
