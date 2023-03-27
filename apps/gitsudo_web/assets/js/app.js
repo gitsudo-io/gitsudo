06f1f07b040bb1f91ec6b3165e3e7ed8f1782e2a
Alistair Israel <aisrael@gmail.com>
2023-03-27 19:57:28 -0400

    Factored out the spinner

--- apps/gitsudo_web/assets/js/app.js
+++ apps/gitsudo_web/assets/js/app.js
@@ -59,16 +59,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
-window.onload = async function () {
-    const targetId = 'Greeter';
-    const target = document.getElementById(targetId);
-
-    if (!target) {
-        return;
-    }
-
-    let props = {};
-
-    const component = new Greeter({ target, props });
-}
