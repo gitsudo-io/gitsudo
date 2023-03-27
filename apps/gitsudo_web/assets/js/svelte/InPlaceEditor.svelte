72122796ff47a05c69b676842dfe4a5913820034
Alistair Israel <aisrael@gmail.com>
2023-03-27 15:48:55 -0400

    Added a submit indicator

--- apps/gitsudo_web/assets/js/svelte/InPlaceEditor.svelte
+++ apps/gitsudo_web/assets/js/svelte/InPlaceEditor.svelte
@@ -6,6 +6,8 @@
     let value = text;
     let submitDisabled = true;
 
+    let submitPromise = null;
+
     function toggleEditing(e) {
         editing = !editing;
     }
@@ -15,8 +17,9 @@
         console.log(submitDisabled);
     }
 
-    async function renameLabel(e) {
+    async function submitForm(e) {
         if (value != text) {
+            console.log("submitPromise: " + submitPromise);
             const url = location.origin + "/api/org/" + org + "/labels/" + id;
             const body = JSON.stringify({
                 label: {
@@ -24,25 +27,27 @@
                 },
             });
             console.log(body);
-            const data = await fetch(url, {
+            submitPromise = fetch(url, {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
                 body,
             }).then((resp) => {
+                console.log("submitPromise: " + submitPromise);
                 let data = resp.json();
                 console.log(JSON.stringify(data));
+                window.location =
+                    location.origin + "/" + org + "/labels/" + value;
+                submitPromise = null;
                 data;
             });
-            console.log(JSON.stringify(data));
-            window.location = location.origin + "/" + org + "/labels/" + value;
         }
         editing = !editing;
     }
 </script>
 
-<div class="align-bottom">
+<div class="align-middle">
     {#if editing}
-        <form on:submit|preventDefault={renameLabel}>
+        <form on:submit|preventDefault={submitForm}>
             <input
                 type="text"
                 on:input={textChanged}
@@ -68,9 +73,80 @@
     {:else}
         <span class="text-2xl">{text}</span>
         <button
-            class="btn btn-xs btn-circle btn-ghost text-neutral hover:bg-transparent"
+            class="btn btn-xs btn-ghost text-neutral hover:bg-transparent"
             on:click={toggleEditing}
-            ><span class="hero-pencil-square" />
+        >
+            {#if submitPromise != null}
+                {#await submitPromise}
+                    <svg
+                        class="w-6 h-6 animate-spin text-gray-400"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M12 4.75V6.25"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M17.1266 6.87347L16.0659 7.93413"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M19.25 12L17.75 12"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M17.1266 17.1265L16.0659 16.0659"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M12 17.75V19.25"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M7.9342 16.0659L6.87354 17.1265"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M6.25 12L4.75 12"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M7.9342 7.93413L6.87354 6.87347"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                {:then}
+                    <span class="hero-pencil-square" />
+                {/await}
+            {:else}
+                <span class="hero-pencil-square" />
+            {/if}
         </button>
     {/if}
 </div>
