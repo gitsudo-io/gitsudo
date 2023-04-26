510dbb0f81e239ce9a262a230f7f5d228016cd98
Alistair Israel <aisrael@gmail.com>
2023-04-26 13:11:31 -0400

    Needs a cassette

--- apps/gitsudo_web/test/gitsudo_web/controllers/api/repo_label_controller_test.exs
+++ apps/gitsudo_web/test/gitsudo_web/controllers/api/repo_label_controller_test.exs
@@ -1,4 +1,5 @@
 defmodule GitsudoWeb.API.RepoLabelControllerTest do
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Finch
   use GitsudoWeb.ConnCase
 
   import Gitsudo.LabelsFixtures
@@ -37,35 +38,35 @@ defmodule GitsudoWeb.API.RepoLabelControllerTest do
     label = label_fixture()
     Logger.debug("label: #{inspect(label)}")
 
-    Gitsudo.Repositories.add_label_to_repository(repository, label)
-
-    access_token = System.get_env("TEST_PERSONAL_ACCESS_TOKEN", @dummy_personal_access_token)
-    ExVCR.Config.filter_sensitive_data(access_token, @dummy_personal_access_token)
+    {:ok, r} = Gitsudo.Repositories.add_label_to_repository(repository, label)
+    Logger.debug("r: #{inspect(r)}")
 
     conn =
       conn
       |> put_req_header("accept", "application/json")
-      |> Plug.Test.init_test_session(access_token: access_token)
+      |> Plug.Test.init_test_session(access_token: @dummy_personal_access_token)
 
     {:ok, conn: conn, label: label}
   end
 
   describe "index" do
-    test "lists all labels", %{conn: conn, label: label} do
-      conn = get(conn, ~p"/api/org/gitsudo-io/gitsudo/labels")
-      assert json_response(conn, 200)
-      assert data = json_response(conn, 200)["data"]
-      assert length(data) > 0
+    test "lists repo labels", %{conn: conn, label: label} do
+      use_cassette("lists_repo_labels_works") do
+        conn = get(conn, ~p"/api/org/gitsudo-io/gitsudo/labels")
+        assert json_response(conn, 200)
+        assert data = json_response(conn, 200)["data"]
+        assert length(data) > 0
 
-      assert data == [
-               %{
-                 "id" => label.id,
-                 "color" => "label-red",
-                 "name" => "backend",
-                 "owner_id" => 121_780_924,
-                 "collaborator_policies" => []
-               }
-             ]
+        assert data == [
+                 %{
+                   "id" => label.id,
+                   "color" => "label-red",
+                   "name" => "backend",
+                   "owner_id" => 121_780_924,
+                   "collaborator_policies" => []
+                 }
+               ]
+      end
     end
   end
 end
