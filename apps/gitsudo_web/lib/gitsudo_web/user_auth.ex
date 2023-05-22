2d701d72363fb54281f1101bb2a24747cb1c344d
Alistair Israel <aisrael@gmail.com>
2023-05-22 14:32:34 -0400

    Don't flash on / or /login

--- apps/gitsudo_web/lib/gitsudo_web/user_auth.ex
+++ apps/gitsudo_web/lib/gitsudo_web/user_auth.ex
@@ -67,15 +67,23 @@ defmodule GitsudoWeb.UserAuth do
       conn
     else
       conn
-      |> put_flash(:error, "You must log in to access this page.")
       |> maybe_store_return_to()
       |> redirect(to: ~p"/login")
       |> halt()
     end
   end
 
-  defp maybe_store_return_to(%{method: "GET"} = conn) do
-    put_session(conn, :user_return_to, current_path(conn))
+  defp maybe_store_return_to(%{method: "GET", request_path: "/login"} = conn), do: conn
+
+  defp maybe_store_return_to(%{method: "GET", request_path: "/"} = conn),
+    do:
+      conn
+      |> put_session(:user_return_to, current_path(conn))
+
+  defp maybe_store_return_to(%{method: "GET", request_path: request_path} = conn) do
+    conn
+    |> put_session(:user_return_to, current_path(conn))
+    |> put_flash(:error, "You must log in to access this page.")
   end
 
   defp maybe_store_return_to(conn), do: conn
