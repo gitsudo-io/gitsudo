ceb814351edff0c358075adccd929a5714b43f36
Alistair Israel <aisrael@gmail.com>
2023-05-22 14:21:49 -0400

    Try to make prod link point to actual

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :gitsudo_web, GitsudoWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "gitsudo.io", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
